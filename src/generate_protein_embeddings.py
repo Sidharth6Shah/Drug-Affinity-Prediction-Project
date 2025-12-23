7ef8067222f4bc40f591d894c2791dff9b32150c
sidharth-k-shah <sidharthkshah@gmail.com>
2025-12-23 17:21:09 -0500

    prepping proteins for embedding generation

--- src/generate_protein_embeddings.py
+++ src/generate_protein_embeddings.py
@@ -2,3 +2,7 @@ import pandas as pd
 import numpy as np
 
 train_df = pd.read_csv('data/train_proteins.csv')
+
+#Some proteins can be associated with multiple different ligands, so this cuts down on extra embedding generation for repeated proteins
+unique_proteins = train_df['protein_sequence'].unique()
+
