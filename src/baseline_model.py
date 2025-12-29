b891f2d6604de1e9a123af453f42d9492ad3d752
sidharth-k-shah <sidharthkshah@gmail.com>
2025-12-29 01:46:19 -0500

    review tmr, making and training model

--- src/baseline_model.py
+++ src/baseline_model.py
@@ -11,3 +11,16 @@ X_val = np.load('data/final/X_val.npy')
 Y_val = np.load('data/final/Y_val.npy')
 X_test = np.load('data/final/X_test.npy')
 Y_test = np.load('data/final/Y_test.npy')
+
+#Set up the model
+model = XGBRegressor(
+    n_estimators=100,
+    max_depth=6,
+    learning_rate=0.1,
+    random_state=42,
+    n_jobs=-1,
+    verbosity=1
+    )
+
+#Train
+model.fit(X_train, Y_train)
