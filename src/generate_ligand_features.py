8e826c38f09d5f31c0280b23ce651078adb8a10c
sidharth-k-shah <sidharthkshah@gmail.com>
2025-12-28 19:16:45 -0500

    FIX THIS WHOLE THING PLEASE

--- src/generate_ligand_features.py
+++ src/generate_ligand_features.py
@@ -25,6 +25,24 @@ def parseSMILES(smiles):
 # Step 3: Generate Morgan fingerprints
 #   Generate Morgan (ECFP) fingerprints for each ligand
 
+from rdkit.Chem import AllChem
+
+def getMFP(smiles, radius = 2, n_bits=2048):
+    #look 2 bonds/connections out, standard size 2048 bits
+    mol = Chem.MolFromSmiles(smiles)
+
+#    if mol is None:
+#        return np.zeros(n_bits)
+    
+    fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius=radius, nBits=n_bits)
+
+#Convert returned rdkit value to numpy for ml
+    arr = np.zeros(n_bits)
+    for i in range (n_bits):
+        arr[i] = fp[i]
+       
+    return arr
+
 # Step 4: (Optional) Add molecular descriptors
 #   Optionally add basic molecular descriptors
 
